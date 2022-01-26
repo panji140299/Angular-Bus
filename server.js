624966e29dad5c548b8b35db88f99a155568a210
panji140299 <panjiprasetya999@gmmail.com>
2022-01-26 13:40:05 +0700

    Server

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 function requireHTTPS(req,res,next){
-    if(!req.secure && req.get('x-forwarded-proto') !== 'https'){
-      return res.redirect('https://'+req.get('host')+req.url)
-    }
-    next()
+  if(!req.secure && req.get('x-forwarded-proto') !== 'https'){
+    return res.redirect('https://'+req.get('host')+req.url)
   }
-  const express = require('express')
-  const app = express()
-  app.use(requireHTTPS)
-  app.use(express.static('./dist/angular12-jwt-auth'))
-  app.get('/*', (req,res)=>
-    res.sendFile('index.html',{root: 'dist/angular12-jwt-auth/'})
-  )
-  const port = process.env.PORT || 8080
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+  next()
+}
+
+const express = require('express')
+const app = express()
+
+app.use(requireHTTPS)
+app.use(express.static('./dist/angular12-jwt-auth'))
+
+app.get('/*', (req,res)=>
+  res.sendFile('index.html',{root: 'dist/angular12-jwt-auth/'})
+)
+
+const port = process.env.PORT || 8082
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
